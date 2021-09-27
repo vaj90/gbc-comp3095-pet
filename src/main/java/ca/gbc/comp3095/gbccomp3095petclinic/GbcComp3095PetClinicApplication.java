99fa3cfb77fc4c0bbe4139b8fbce9eb32fe804de
Allan John Valiente <allanjohn.valiente@gmail.com>
2021-09-27 15:28:56 -0400

    Added files

---
+++ src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/GbcComp3095PetClinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbccomp3095petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcComp3095PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbcComp3095PetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/comp3095/gbccomp3095petclinic/GbcComp3095PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbccomp3095petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcComp3095PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
